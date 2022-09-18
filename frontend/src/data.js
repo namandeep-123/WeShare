91302d06be1bc0deeb6a1049e72dbd666cc39414
goyalkamal <kamalgoyal036@gmail.com>
2022-09-18 12:37:33 +0530

    Create blog page,Product Card & add Product Page

---
+++ frontend/src/data.js
@@ -0,0 +1,44 @@
+[
+    {
+        img:"https://www.flaticon.com/free-icon/smartphone_488?term=phone&page=1&position=13&page=1&position=13&related_id=488&origin=style",
+        title:"Sell Scrap ",
+        content:"You can sell your waste products at good prices.",
+        link:"#",
+        button:"read now",
+    },
+    {
+        img:"https://www.flaticon.com/free-icon/paper-shredder_4210866?term=shredding&page=1&position=10&page=1&position=10&related_id=4210866&origin=style",
+        title:"Shredding service",
+        content:"Aiding Businesses in the safe & secure disposal of their confidential documents.",
+        link:"#",
+        button:"read now",
+    },
+    {
+        img:"https://www.flaticon.com/free-icon/smartphone_488?term=phone&page=1&position=13&page=1&position=13&related_id=488&origin=style",
+        title:"Zero waste services",
+        content:"Serving the Institutes/Offices/Events in achieving their zero waste goals.",
+        link:"#",
+        button:"read now",
+    },
+    {
+        img:"https://www.flaticon.com/free-icon/snow_6268254?term=remove&page=1&position=47&page=1&position=47&related_id=6268254&origin=style",
+        title:"Dismantling service",
+        content:"Providing holistic approach to implement circular solutions to the scrap disposal.",
+        link:"#",
+        button:"read now",
+    },
+    {
+        img:"https://www.flaticon.com/free-icon/recovery_3759849?term=recovery&page=1&position=66&page=1&position=66&related_id=3759849&origin=style",
+        title:"Material Recovery Facility",
+        content:"Collaborating to provide waste management services to ULB owned Material Recovery Facilities.",
+        link:"#",
+        button:"read now",
+    },
+    {
+        img:"https://www.flaticon.com/free-icon/donation_2618479?term=donation&page=1&position=2&page=1&position=2&related_id=2618479&origin=style",
+        title:"Donating platform",
+        content:"You can donate here anything you want to donate in any much qunatity you want",
+        link:"#",
+        button:"read now",
+    }
+]
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Blog from './Blog';
 import reportWebVitals from './reportWebVitals';
-
+import Sell from './Sell';
+import Buy from './Buy';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Blog/>
+    <Sell/>
+    <Buy/>
+
   </React.StrictMode>
 );
 
