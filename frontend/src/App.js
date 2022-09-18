55aef0c26bb2e51140f30ad103800dd23cb2ef8d
Om Gupta <om@Oms-MacBook-Air.local>
2022-09-18 12:34:40 +0530

    Create Scrap Pricing page.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,41 @@
-import { render } from "react-dom";
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-function App() {
+
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+
+// Pages
+import Home from "./pages/Home";
+import ScrapPricing from "./pages/ScrapPricing";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#27ba32",
+      light: "#4caf50",
+      dark: "#27ba32",
+      contrastText: "#fff",
+    },
+  },
+});
+
+const App = () => {
   return (
     <>
-      <BrowserRouter></BrowserRouter>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+
+        <BrowserRouter>
+          <Routes>
+            <Route path="/">
+              <Route index element={<Home />} />
+              <Route path="scrap-pricing" element={<ScrapPricing />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
     </>
   );
-}
+};
 
 export default App;
